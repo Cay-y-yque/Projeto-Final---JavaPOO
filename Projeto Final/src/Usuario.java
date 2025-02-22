e113464e843640836b1817bf9ea0e6180dfd9996
Kragdom <162316518+Kragdom@users.noreply.github.com>
2025-02-22 20:01:39 -0300

    Add files via upload

---
+++ Projeto Final/src/Usuario.java
@@ -0,0 +1,35 @@
+package com.equipe.POO_projetoFinal.main;
+
+public abstract class Usuario {
+    private String nome;
+    private int idade;
+    private String[] listaAnimais;
+    
+    public Usuario(String nome, int idade){
+    	this.idade = idade;
+    	this.nome = nome;
+    }
+    
+    public abstract void registrar();
+    
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String[] getListaAnimais() {
+		return listaAnimais;
+	}
+	public void setListaAnimais(String[] listaAnimais) {
+		this.listaAnimais = listaAnimais;
+	}
+}
