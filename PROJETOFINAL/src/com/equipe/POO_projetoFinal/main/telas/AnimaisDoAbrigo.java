779530259a5989a0c3514d81cbe5a7cad5d5bed6
Cay-y-yque <cayquecagas666@outlook.com>
2025-02-24 21:56:32 -0300

    Update AnimaisDoAbrigo.java

--- PROJETOFINAL/src/com/equipe/POO_projetoFinal/main/telas/AnimaisDoAbrigo.java
+++ PROJETOFINAL/src/com/equipe/POO_projetoFinal/main/telas/AnimaisDoAbrigo.java
@@ -1,13 +1,14 @@
 package com.equipe.POO_projetoFinal.main.telas;
 
-import java.awt.EventQueue;
 
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.util.ArrayList;
-import java.util.List;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import com.equipe.POO_projetoFinal.main.Animal;
@@ -15,65 +16,67 @@ import com.equipe.POO_projetoFinal.main.Animal;
 
 public class AnimaisDoAbrigo extends JFrame{
 	private static final long serialVersionUID = 1L;
-	private List<JButton> botoesAnimais = new ArrayList<>();
+	private ArrayList<JButton> botoesAnimais = new ArrayList<>();
 	
 	public AnimaisDoAbrigo() {
-		super.setDefaultCloseOperation(EXIT_ON_CLOSE);;
-		super.setSize(740, 800);
-		super.setLocationRelativeTo(null);
-		super.setTitle("Lista de Animais");
+		// Setando os atributos do frame
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);;
+		this.setSize(740, 800);
+		this.setResizable(false);
+		this.setLocationRelativeTo(null);
+		this.setTitle("Lista de Animais");
+		// 
 		
+		//Criando um container para ser o container principal
 		JPanel conteudo = new JPanel();
-		conteudo.setLayout(null);
-		super.setContentPane(conteudo);
+		conteudo.setLayout(null); // Deixando o layout livre para poder colocar os componentes manualmente
+		this.setContentPane(conteudo);
+		//
 		
+		//Criando uma área para ser o cabeçalho da tela
 		JLabel cabecalho = new JLabel("Animais do Abrigo");
 		cabecalho.setBounds(210, 0, 400, 80);
 		cabecalho.setFont(new Font(" ", Font.PLAIN, 40));
 		conteudo.add(cabecalho);
+		//
 		
-		Animal animal1 = new Animal("Pano", 5, "B", "Chato", 85.2, "Saudavel", 33.3);
-		Animal animal2 = new Animal("janas", 13, "Ba", "Legal", 85.2, "Saudavel", 33.3);
-		Animal animal3 = new Animal("Pacas", 12, "Ban", "Burro", 85.2, "Saudavel", 33.3);
-		Animal animal4 = new Animal("Lírio", 1, "Bana", "Inteligente", 85.2, "Saudavel", 33.3);
-		Animal animal5 = new Animal("Lóbulo", 50, "Banan", "Sábio", 85.2, "Saudavel", 33.3);
-		Animal animal6 = new Animal("Rinoceronte", 4, "Banana", "Emocionado", 85.2, "Saudavel", 33.3);
+		//Criando um painel que vai representar a área onde os botões de cada animal vão ficar
+		JPanel buttonArea = new JPanel();
+		buttonArea.setLayout(new FlowLayout(FlowLayout.LEFT, 20 , 10));
+		int alturaInterior = (Animal.getListaAnimais().size() * 40 / 3) + 40;
+		buttonArea.setPreferredSize(new Dimension(640, alturaInterior));
+		//
 		
-		Animal.getListaAnimais().add(animal1); Animal.getListaAnimais().add(animal2); Animal.getListaAnimais().add(animal3);
-		Animal.getListaAnimais().add(animal4); Animal.getListaAnimais().add(animal5); Animal.getListaAnimais().add(animal6);
+		
 		
 		JButton botao = null;
-		int x = 30;
-		int y = 80;
-
+		//Loop que vai percorrer cada animal da lista e fazer um botão específico para ele.
 		for (int i = 1; i <= Animal.getListaAnimais().size(); i++) {
-			if (y >= 800) {
-				break;
-			}
 
 			final Animal animal = Animal.getListaAnimais().get(i-1);
-			botao = new JButton("Nome: " + animal.getNome() + " | " + "ID: " + animal.getId());
+			botao = new JButton("ID: " + animal.getId() + " | " + "Nome: " + animal.getNome());
+			botao.setPreferredSize(new Dimension(200, 30));
+
 			botao.addActionListener(e -> criarTelaDoAnimal(animal.getId()));
-			botao.setBounds(x, y, 200, 20);
 			
-			conteudo.add(botao);
+			buttonArea.add(botao);
 			botoesAnimais.add(botao);
-			
-			x+= 230;
-			if (i % 3 == 0) {
-				x = 30;
-				y += 30;
-			}
-			
+
 		}
+		//
 		
-		
-		
+		// Vai criar um painel de scroll que vai mostrar o componente buttonArea.
+		JScrollPane scrollParaBotoes = new JScrollPane(buttonArea);
+		scrollParaBotoes.setBounds(14, 80, 700, 680);
+		scrollParaBotoes.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		conteudo.add(scrollParaBotoes); // adiciona o scroll no painel principal
+		//
 	
 	}
 	
+	// Função que vai ser chamada quando um botão de um animal for clicado.
 	public void criarTelaDoAnimal(int id) {
-		TelaDoAnimal telaA = new TelaDoAnimal(id);
+		TelaDoAnimal telaA = new TelaDoAnimal(id); // ela instancia uma tela e deixa ela visivel.
 		telaA.setVisible(true);
 	}
 	
